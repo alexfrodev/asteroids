131e447a19fd185138ce5ceecc64ff431ecd7f77
alexfrodev <alex.franco3d@outlook.com>
2024-09-12 23:09:09 -0600

    add functional black window

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ print("Starting asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
+
 if __name__ == "__main__":
     main()
