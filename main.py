4b08009a06a7d81986f01f51e23573a110366532
alexfrodev <alex.franco3d@outlook.com>
2024-09-23 19:31:51 -0600

    draw a white triangle as player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 print("Starting asteroids!")
 
@@ -13,6 +14,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     fps = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -20,9 +23,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
         dt = fps.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
