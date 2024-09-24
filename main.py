dcdb816d5934473ae8a3f1d95e951a15b8612ddc
alexfrodev <alex.franco3d@outlook.com>
2024-09-24 17:37:24 -0600

    added asteroids floating around

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 print("Starting asteroids!")
 
@@ -15,10 +17,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     
     dt = 0
 
@@ -32,7 +38,6 @@ def main():
         for object in updatable:
             object.update(dt)
         for object in drawable:
-            print(object)
             object.draw(screen)
 
         pygame.display.flip()
